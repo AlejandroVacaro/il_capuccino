0e6325fc06b7e781fe2f5e6ad04b299d48902925
Alejandro Vacaro <avacaro@outlook.com>
2023-02-07 00:35:59 -0300

    Se agrega ícono de carrito de compras a la barra de navegación.

---
+++ src/components/CartWidget.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const CardWidget = () => {
+  return (
+    <>
+    <li>
+      <a href="#">
+        <i class="material-icons">
+          <ShoppingCartIcon />
+        </i>
+      </a>
+    </li>
+    </>
+  );
+};
+
+export default CardWidget;
