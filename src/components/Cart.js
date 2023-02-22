ae0647561c05087e6bf6855d0ecb5bca8a364c14
Alejandro Vacaro <avacaro@outlook.com>
2023-02-22 02:02:45 -0300

    Se mejora vista en componentes Cart y NotFoundPage.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,10 +2,26 @@ import React from "react"
 
 const Cart = () => {
   return (
-    <div>
-        <h1>Sitio en construcción.</h1>
+    <div style={style.divContruccion}>
+      <h1 style={style.sitioConstruccion}>Sitio en construcción.</h1>
+      <img style={style.imgConstruccion} src="https://static.vecteezy.com/system/resources/previews/006/796/852/non_2x/sad-coffee-character-in-flat-style-illustration-coffee-cup-cartoon-character-with-funny-expression-free-vector.jpg" />
     </div>
   )
 };
 
 export default Cart
+
+const style = {
+  divContruccion: {
+    display:"flex",
+    flexDirection:"column",
+    alignItems: "center"
+  },
+  sitioConstruccion: {
+    marginTop: "60px",
+  },
+  imgConstruccion: {
+    width:"40%",
+    height: "auto"
+  }
+}
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React from "react"
 
 const NotFoundPage = () => {
-    return (
-        <>
-            <p>Página no encontrada</p>
-        </>
-    )
-}
+  return (
+    <div style={style.divNotFoundPage}>
+      <h1 style={style.sitioNotFoundPage}>Sitio no encontrado.</h1>
+      <img style={style.imgNotFoundPage} src="https://static.vecteezy.com/system/resources/previews/006/796/852/non_2x/sad-coffee-character-in-flat-style-illustration-coffee-cup-cartoon-character-with-funny-expression-free-vector.jpg" />
+    </div>
+  )
+};
 
 export default NotFoundPage;
+
+const style = {
+  divNotFoundPage: {
+    display:"flex",
+    flexDirection:"column",
+    alignItems: "center"
+  },
+  sitioNotFoundPage: {
+    marginTop: "60px",
+  },
+  imgNotFoundPage: {
+    width:"40%",
+    height: "auto"
+  }
+}
